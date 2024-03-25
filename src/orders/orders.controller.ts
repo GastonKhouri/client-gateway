fdbca1705666bdd38caa94a0f97010f8462f4253
Gaston Khouri <gastonkhouri@gmail.com>
2024-03-25 11:40:01 -0400

    Add Docker configuration and exception handling

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
-
-
-import { NATS_SERVICE, ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { firstValueFrom } from 'rxjs';
@@ -20,8 +18,16 @@ export class OrdersController {
 	}
 
 	@Get()
-	findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
-		return this.client.send( 'findAllOrders', orderPaginationDto );
+	async findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
+		try {
+			const orders = await firstValueFrom(
+				this.client.send( 'findAllOrders', orderPaginationDto )
+			);
+			return orders;
+
+		} catch ( error ) {
+			throw new RpcException( error );
+		}
 	}
 
 	@Get( 'id/:id' )
